9513ea09f20386cb27645c5c5531700cabd86edd
Shah, Piyushkumar <PTS6@pge.com>
2018-09-04 05:47:58 -0700

    mocha testing

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const express = require('express')
 const app = express()
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World'))
 app.listen(3000, () => console.log('Example app listening on 3000 port'))
