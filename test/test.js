d104711dfb1ab4be4b1664e2f2e141611a475197
Shah, Piyushkumar <PTS6@pge.com>
2018-09-04 12:43:17 -0700

    Mocha report

--- test/test.js
+++ test/test.js
@@ -2,8 +2,11 @@ var expect  = require('chai').expect;
 var request = require('request');
 
 if('Main page content', function(done) {
+	console.log('1');
     request('http://localhost:3000' , function(error, response, body) {
+		console.log('2');
         expect(body).to.equal('Hello World');
+        console.log('3');
         done();
     });
 });
